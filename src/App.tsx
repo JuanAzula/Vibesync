c12b9ad93ee42beccc71b77c37f600a4f3ea7b7d
JuanAzula <141027910+JuanAzula@users.noreply.github.com>
2024-03-01 08:54:12 +0100

    useQuery example

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
 import './styles/App.css'
+import { useQuery } from '@tanstack/react-query'
+import { getTracks } from './services/tracksService'
 
 export const App = () => {
+  const query = useQuery({
+    queryKey: ['tracks'],
+    queryFn: async () => await getTracks()
+  })
+
+  console.log(query.data)
+
   return (
     <>
+    <div>
+      <h1>Tracks</h1>
+      <ul>
+        {query.data?.map((track) => (
+          <li key={track.id}>{track.name}</li>
+        ))}
+      </ul>
+    </div>
     </>
   )
 }
