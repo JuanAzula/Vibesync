ed8e6d756486217f170d1e3b10129ebb4d54b6e1
Alba Martin <albams51289@gmail.com>
2024-03-01 09:57:22 +0100

    Home page ready to work in it

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Home from "./pages/Home/index.tsx";
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={new QueryClient()}>
-    <App />
+      <Home />
     </QueryClientProvider>
   </React.StrictMode>
-)
+);
--- src/router/AppRoutes.tsx
+++ src/router/AppRoutes.tsx
@@ -1,19 +1,20 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "../pages/Home";
 
 export const AppRoutes = () => {
   return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/"
-                //  element={<Home />}
-                 />
-                <Route path="/login"
-                //  element={<Login />}
-                 />
-                <Route path="/register"
-                //  element={<Register />}
-                 />
-            </Routes>
-        </BrowserRouter>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/login"
+          //  element={<Login />}
+        />
+        <Route
+          path="/register"
+          //  element={<Register />}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
+};
