9c8389594566fc64ee393879aeaa5d324e10d180
JuanAzula <141027910+JuanAzula@users.noreply.github.com>
2024-03-04 16:58:28 +0100

    Add song player

--- src/pages/SongPage/index.tsx
+++ src/pages/SongPage/index.tsx
@@ -28,7 +28,6 @@ export const SongPage = () => {
     queryFn: async () => await getTrack(trackId)
   })
   console.log('queryTrack', queryTrack.data)
-
   const audioRef = useRef<HTMLAudioElement>(null)
   const [isPlaying, setIsPlaying] = useState(false)
   const [songDuration, setSongDuration] = useState('00:00')
@@ -37,9 +36,9 @@ export const SongPage = () => {
   const [progressWidth, setProgressWidth] = useState('0%')
 
   /// /Cuando tengáis las canciones y les redireccione aquí, este código servirá para reproducir la canción de manera automática (CREO) porque el usuario ya habrá interactuado. Poneis el initialValue del isPlaying en TRUE, y este useEffect
-  // useEffect(() => {
-  //   if (isPlaying && audioRef.current) audioRef.current.play();
-  // },[isPlaying])
+  useEffect(() => {
+    if (isPlaying && audioRef.current) audioRef.current.play()
+  }, [isPlaying])
 
   /// esto junto con el audioRef pódeis meterlo en un custom Hook si queréis (usePlay), porque lo utilizaréis más veces. No lo hago por no liar
   const togglePlay = () => {
@@ -61,7 +60,7 @@ export const SongPage = () => {
     }
   }
 
-  /// lo mismo con esta función, metedla en una carpeta de utils/globals para limpiar el código. Lo dejo aquí para que os sea más fácil entederlo. Explicación-> Simplemente coges la duraciónm, que te la dan en segundos, y la separas en minutos y segundos.
+  // /// lo mismo con esta función, metedla en una carpeta de utils/globals para limpiar el código. Lo dejo aquí para que os sea más fácil entederlo. Explicación-> Simplemente coges la duraciónm, que te la dan en segundos, y la separas en minutos y segundos.
   const getSongDuration = (audioRef: React.RefObject<HTMLAudioElement>, setSongDuration: React.Dispatch<React.SetStateAction<string>>) => {
     if (audioRef.current && audioRef.current.duration) {
       const duration = audioRef.current?.duration
@@ -81,11 +80,11 @@ export const SongPage = () => {
   }
 
   /// Esta función sirve para formatear el currentTime, recibe el currentTime por parametro y lo formatea en min y sec para enseñarlo en la pantalla. LO MISMO, esto, idealmente, en UTILS/GLOBALS, no aquí
-  // const formatTime = (timeInSeconds: number): string => {
-  //   const minutes = Math.floor(timeInSeconds / 60);
-  //   const seconds = Math.floor(timeInSeconds % 60).toString().padStart(2, '0');
-  //   return `${minutes}:${seconds}`;
-  // };
+  const formatTime = (timeInSeconds: number): string => {
+    const minutes = Math.floor(timeInSeconds / 60)
+    const seconds = Math.floor(timeInSeconds % 60).toString().padStart(2, '0')
+    return `${minutes}:${seconds}`
+  }
 
   /// esto es para poder ir a la parte de la canción que queráis clickando. Si no entendeis la linea 76 es normal. YO TAMPOCO JEJEJE YUHUUUUU
   const handleProgressClick = (e: React.MouseEvent<HTMLDivElement>) => {
@@ -100,32 +99,32 @@ export const SongPage = () => {
   }
 
   // sin este useEffect intenta coger la duración de la canción antes de que se carguen los metadatos de la canción, por lo que no coge la duración. Aquí simplemente pues escucha a que se carguen los metadatos pa ejecutar la funcion getSongDuration, capici? Ale pues
-  // useEffect(() => {
-  //   const handleLoadedMetadata = () => {
-  //     getSongDuration(audioRef, setSongDuration)
-  //   };
+  useEffect(() => {
+    const handleLoadedMetadata = () => {
+      getSongDuration(audioRef, setSongDuration)
+    }
 
-  //   if (audioRef.current) {
-  //     audioRef.current.addEventListener('loadedmetadata', handleLoadedMetadata);
-  //     audioRef.current.addEventListener('timeupdate', handleTimeUpdate);
-  //     return () => {
-  //       audioRef.current?.removeEventListener('loadedmetadata', handleLoadedMetadata);
-  //       audioRef.current?.removeEventListener('timeupdate', handleTimeUpdate);
-  //     };
-  //   }
-  // }, []);
+    if (audioRef.current) {
+      audioRef.current.addEventListener('loadedmetadata', handleLoadedMetadata)
+      audioRef.current.addEventListener('timeupdate', handleTimeUpdate)
+      return () => {
+        audioRef.current?.removeEventListener('loadedmetadata', handleLoadedMetadata)
+        audioRef.current?.removeEventListener('timeupdate', handleTimeUpdate)
+      }
+    }
+  }, [])
 
   return (
       <>
-        <SongDetails song={queryTrack.data} />
-        {/* <audio ref={audioRef} src={song.url} /> */}
+        <SongDetails song={queryTrack.data ? queryTrack.data : {}} />
+        <audio ref={audioRef} src={queryTrack.data ? queryTrack.data.url : {}} />
         <h1>Song</h1>
         <div className="progress-bar" onClick={handleProgressClick}>
           <div className="progress" style={{ width: progressWidth }}></div>
           <div className="progress-indicator" style={{ left: progressWidth }}></div>
         </div>
         <div className="duration-container">
-          {/* <span>{formatTime(currentTime)}</span> */}
+          <span>{formatTime(currentTime)}</span>
           <span>{songDuration}</span>
         </div>
         <PlayButtons toggleMute={toggleMute} togglePlay={togglePlay} isPlaying={isPlaying} isMuted={isMuted} />
