482885fcaa8b3f386b02fbe88eede07ae081ae33
aoteron <aoteronunez@gmail.com>
2024-03-05 19:30:57 +0100

    style LibraryPage & upload assets

--- src/pages/LibraryPage/index.tsx
+++ src/pages/LibraryPage/index.tsx
@@ -1,5 +1,11 @@
 import './LibraryPage.css'
 import CategoryBtn from '../../styledComponents/categoryBtn'
+import likedSongsImg from '../../assets/default-mylibrary/likedsongs.png'
+import newEpisodesImg from '../../assets/default-mylibrary/newepisodes.png'
+import myLifeIsAMovieImg from '../../assets/default-mylibrary/mylifeisamovie.png'
+import topSongsImg from '../../assets/default-mylibrary/topsong2023.png'
+import acousticChillImg from '../../assets/default-mylibrary/acousticchill.png'
+import amourDeIyceeImg from '../../assets/default-mylibrary/amourdeiycee.png'
 
 export default function LibraryPage () {
   return (
@@ -13,6 +19,135 @@ export default function LibraryPage () {
             </div>
         </section>
         <main className='library-main-page'>
+        <ul className='library-main-page__list'>
+          <li>
+            <img src={likedSongsImg} alt='Liked Songs' />
+            <div>
+              <h3>Liked Songs</h3>
+              <p>Subtitle text here</p>
+            </div>
+          </li>
+          <li>
+            <img src={newEpisodesImg} alt='New Episodes' />
+            <div>
+              <h3>New Episodes</h3>
+              <p>Subtitle text here</p>
+            </div>
+          </li>
+          <li>
+            <img src={myLifeIsAMovieImg} alt='My Life is a Movie' />
+            <div>
+              <h3>My Life is a Movie</h3>
+              <p>Subtitle text here</p>
+            </div>
+          </li>
+          <li>
+            <img src={topSongsImg} alt='Your Top Songs 2023' />
+            <div>
+              <h3>Your Top Songs 2023</h3>
+              <p>Subtitle text here</p>
+            </div>
+          </li>
+          <li>
+            <img src={acousticChillImg} alt='Acoustic Chill' />
+            <div>
+              <h3>Acoustic Chill</h3>
+              <p>Subtitle text here</p>
+            </div>
+          </li>
+          <li>
+            <img src={amourDeIyceeImg} alt='Amour de Iycee' />
+            <div>
+              <h3>Amour de Iycee</h3>
+              <p>Subtitle text here</p>
+            </div>
+          </li>
+          {/* Restarting hardcode list */}
+          <li>
+            <img src={likedSongsImg} alt='Liked Songs' />
+            <div>
+              <h3>Liked Songs</h3>
+              <p>Subtitle text here</p>
+            </div>
+          </li>
+          <li>
+            <img src={newEpisodesImg} alt='New Episodes' />
+            <div>
+              <h3>New Episodes</h3>
+              <p>Subtitle text here</p>
+            </div>
+          </li>
+          <li>
+            <img src={myLifeIsAMovieImg} alt='My Life is a Movie' />
+            <div>
+              <h3>My Life is a Movie</h3>
+              <p>Subtitle text here</p>
+            </div>
+          </li>
+          <li>
+            <img src={topSongsImg} alt='Your Top Songs 2023' />
+            <div>
+              <h3>Your Top Songs 2023</h3>
+              <p>Subtitle text here</p>
+            </div>
+          </li>
+          <li>
+            <img src={acousticChillImg} alt='Acoustic Chill' />
+            <div>
+              <h3>Acoustic Chill</h3>
+              <p>Subtitle text here</p>
+            </div>
+          </li>
+          <li>
+            <img src={amourDeIyceeImg} alt='Amour de Iycee' />
+            <div>
+              <h3>Amour de Iycee</h3>
+              <p>Subtitle text here</p>
+            </div>
+          </li>
+          <li>
+            <img src={likedSongsImg} alt='Liked Songs' />
+            <div>
+              <h3>Liked Songs</h3>
+              <p>Subtitle text here</p>
+            </div>
+          </li>
+          <li>
+            <img src={newEpisodesImg} alt='New Episodes' />
+            <div>
+              <h3>New Episodes</h3>
+              <p>Subtitle text here</p>
+            </div>
+          </li>
+          <li>
+            <img src={myLifeIsAMovieImg} alt='My Life is a Movie' />
+            <div>
+              <h3>My Life is a Movie</h3>
+              <p>Subtitle text here</p>
+            </div>
+          </li>
+          <li>
+            <img src={topSongsImg} alt='Your Top Songs 2023' />
+            <div>
+              <h3>Your Top Songs 2023</h3>
+              <p>Subtitle text here</p>
+            </div>
+          </li>
+          <li>
+            <img src={acousticChillImg} alt='Acoustic Chill' />
+            <div>
+              <h3>Acoustic Chill</h3>
+              <p>Subtitle text here</p>
+            </div>
+          </li>
+          <li>
+            <img src={amourDeIyceeImg} alt='Amour de Iycee' />
+            <div>
+              <h3>Amour de Iycee</h3>
+              <p>Subtitle text here</p>
+            </div>
+          </li>
+        </ul>
         </main>
         </>
   )
