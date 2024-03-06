a5ef85ffc51e4a80f546d3d93ef6bc1565d681d0
JuanAzula <141027910+JuanAzula@users.noreply.github.com>
2024-03-06 11:36:00 +0100

    Add more songs and fix dependencie issue

--- src/pages/SongPage/components/playButtons/index.tsx
+++ src/pages/SongPage/components/playButtons/index.tsx
@@ -1,7 +1,7 @@
 import './playButtons.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBackwardStep, faBackward, faPlay, faForward, faForwardStep, faPause, faVolumeMute, faVolumeUp, faHeart } from '@fortawesome/free-solid-svg-icons'
-import { faHeart as faHeartRegular } from '@fortawesome/free-regular-svg-icons';
+import { faHeart as faHeartRegular } from '@fortawesome/free-regular-svg-icons'
 import { useState } from 'react'
 
 interface Props {
