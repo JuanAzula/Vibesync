2cf93d563bfe3d30b95a56b4b1f9ab388d4e75e7
JuanAzula <141027910+JuanAzula@users.noreply.github.com>
2024-03-06 17:47:13 +0100

    Add img with link in player

--- src/components/songCard/index.tsx
+++ src/components/songCard/index.tsx
@@ -3,7 +3,6 @@ import './SongCard.css'
 import { useAudioContext } from '../../hooks/useAudio'
 import { useQuery } from '@tanstack/react-query'
 import { getTrack as fetchTrack } from '../../services/dataService'
-import { useState } from 'react'
 
 interface Props {
   track: Track
@@ -11,28 +10,29 @@ interface Props {
 }
 
 const SongCard = ({ track, isActive }: Props) => {
-  const { setAudioUrl } = useAudioContext()
-  const [trackId, setTrackId] = useState<number>()
+  const { setAudioUrl, setAudioImg, trackId, setTrackId } = useAudioContext()
   const getTrack = async (trackId: number | undefined) => {
     console.log(trackId)
     if (trackId) {
       const track = await fetchTrack(trackId)
       console.log('track', track.url)
       setAudioUrl(track.url)
+      setAudioImg(track.thumbnail)
+      localStorage.setItem('localTrack', JSON.stringify(track))
+      return track
+    } else {
+      const track = JSON.parse(localStorage.getItem('localTrack') || '{}')
+      console.log('track', track.url)
+      setAudioUrl(track.url)
+      setAudioImg(track.thumbnail)
       return track
     }
   }
   const queryTrack = useQuery({
     queryKey: ['track'],
     queryFn: async () => await getTrack(trackId)
-    // enabled: !!trackId
   })
 
-  // useEffect(() => {
-  //   if (trackId) {
-  //     void queryTrack.refetch()
-  //   }
-  // }, [trackId])
   console.log('queryTrack', queryTrack.data)
   console.log(trackId)
   return (
@@ -40,13 +40,11 @@ const SongCard = ({ track, isActive }: Props) => {
       setTrackId(track.id)
       void queryTrack.refetch()
     }}>
-      {/* <StyledLink to={`/tracks/${track.id}`}> */}
       <img className="songcard-img" src={track.thumbnail} />
       <div className="songcard-track-info">
         <h3>{track.name}</h3>
         {isActive && <p>{track.artist}</p>}
       </div>
-      {/* </StyledLink> */}
     </div>
   )
 }
