794cb4ad1d6618a3ed06cbf96f64f4d4d5c1a735
aoteron <aoteronunez@gmail.com>
2024-03-07 14:03:25 +0100

    Update main delete AGAIN LikedSongs

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { AppRoutes } from './router/AppRoutes'
 import { AudioProvider } from './context/Audio'
 import { SearchProvider } from './context/Search'
-import { LikedSongsProvider } from './context/LikedSongs'
 import { LikedTracksProvider } from './context/LikedSongs'
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
@@ -13,11 +12,9 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <LikedTracksProvider>
       <AudioProvider>
     <QueryClientProvider client={new QueryClient()}>
-      <LikedSongsProvider>
       <SearchProvider>
       <AppRoutes/>
       </SearchProvider>
-      </LikedSongsProvider>
     </QueryClientProvider>
       </AudioProvider>
     </LikedTracksProvider>
