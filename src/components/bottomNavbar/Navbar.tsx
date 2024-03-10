f3bccba0307567fc01e591f84120aaf476515a4a
aoteron <153718746+aoteron@users.noreply.github.com>
2024-03-10 18:59:23 +0100

    style: :art: navbar
    Navbar buttons now change to an active one when you navigate trough pages

--- src/components/bottomNavbar/Navbar.tsx
+++ src/components/bottomNavbar/Navbar.tsx
@@ -1,32 +1,59 @@
-import { Link } from 'react-router-dom'
+import './navbar.css'
+import { Link, useLocation } from 'react-router-dom'
 import HomeIcon from '../../assets/icons/home-outline.svg'
 import SearchIcon from '../../assets/icons/search-sharp.svg'
 import LibraryIcon from '../../assets/icons/book-outline.svg'
 import ProfileIcon from '../../assets/icons/people-outline.svg'
-import './navbar.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBookOpen, faHouse, faSearch, faUserGroup } from '@fortawesome/free-solid-svg-icons'
 
 const Navbar: React.FC = () => {
+  const location = useLocation()
   return (
     <>
       <nav className="navbar">
         <div className="navbar-item">
           <Link to="/">
-          <img src={HomeIcon} alt="Home button" className="icon" />
+            {location.pathname === '/'
+              ? (
+                <FontAwesomeIcon icon={faHouse} className='icon active' />
+                )
+              : (
+                <img src={HomeIcon} alt="Home button" className="icon" />
+                )}
           </Link>
         </div>
         <div className="navbar-item">
           <Link to="/search">
-          <img src={SearchIcon} alt="Search button" className="icon" />
+            {location.pathname === '/search'
+              ? (
+                <FontAwesomeIcon icon={faSearch} className='icon active' />
+                )
+              : (
+                <img src={SearchIcon} alt="Search button" className="icon" />
+                )}
           </Link>
         </div>
         <div className="navbar-item">
-        <Link to="/library">
-          <img src={LibraryIcon} alt="Library icon" className="icon" />
-        </Link>
+          <Link to="/library">
+            {location.pathname === '/library'
+              ? (
+                <FontAwesomeIcon icon={faBookOpen} className='icon active' />
+                )
+              : (
+                <img src={LibraryIcon} alt="Library icon" className="icon" />
+                )}
+          </Link>
         </div>
         <div className="navbar-item">
           <Link to="/user">
-          <img src={ProfileIcon} alt="User icon" className="icon" />
+            {location.pathname === '/user'
+              ? (
+                <FontAwesomeIcon icon={faUserGroup} className='icon active' />
+                )
+              : (
+                <img src={ProfileIcon} alt="User icon" className="icon" />
+                )}
           </Link>
         </div>
       </nav>
