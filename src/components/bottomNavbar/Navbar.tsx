c3b891036c05bab51d43f852a3692f480177f5c5
aoteron <153718746+aoteron@users.noreply.github.com>
2024-03-10 14:31:19 +0100

    update: :art: responsive userPage, navbar & player

--- src/components/bottomNavbar/Navbar.tsx
+++ src/components/bottomNavbar/Navbar.tsx
@@ -12,22 +12,21 @@ const Navbar: React.FC = () => {
         <div className="navbar-item">
           <Link to="/">
           <img src={HomeIcon} alt="Home button" className="icon" />
-          Home</Link>
+          </Link>
         </div>
         <div className="navbar-item">
           <Link to="/search">
           <img src={SearchIcon} alt="Search button" className="icon" />
-          Search
           </Link>
         </div>
         <div className="navbar-item">
         <Link to="/library">
-          <img src={LibraryIcon} alt="Library icon" className="icon" />My library
+          <img src={LibraryIcon} alt="Library icon" className="icon" />
         </Link>
         </div>
         <div className="navbar-item">
           <Link to="/user">
-          <img src={ProfileIcon} alt="User icon" className="icon" />My profile
+          <img src={ProfileIcon} alt="User icon" className="icon" />
           </Link>
         </div>
       </nav>
