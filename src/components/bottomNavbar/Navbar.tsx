d547aacad256184aa45cb34fd9b0277b16a357a4
aoteron <153718746+aoteron@users.noreply.github.com>
2024-03-10 20:39:43 +0100

    feat: :art: navbar buttons animations

--- src/components/bottomNavbar/Navbar.tsx
+++ src/components/bottomNavbar/Navbar.tsx
@@ -1,19 +1,31 @@
 import './navbar.css'
 import { Link, useLocation } from 'react-router-dom'
+import { motion } from 'framer-motion'
 import HomeIcon from '../../assets/icons/home-outline.svg'
 import SearchIcon from '../../assets/icons/search-sharp.svg'
 import LibraryIcon from '../../assets/icons/book-outline.svg'
 import ProfileIcon from '../../assets/icons/people-outline.svg'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBookOpen, faHouse, faSearch, faUserGroup } from '@fortawesome/free-solid-svg-icons'
+import { useEffect, useState } from 'react'
 
 const Navbar: React.FC = () => {
   const location = useLocation()
+  const [prevPath, setPrevPath] = useState<string>(location.pathname)
+
+  useEffect(() => {
+    setPrevPath(location.pathname)
+  }, [location.pathname])
+
+  const shouldAnimate = (path: string) => {
+    return prevPath !== path ? { scale: 2, transition: { duration: 0.3 } } : {}
+  }
   return (
     <>
       <nav className="navbar">
         <div className="navbar-item">
           <Link to="/">
+          <motion.div whileTap={shouldAnimate('/')}>
             {location.pathname === '/'
               ? (
                 <FontAwesomeIcon icon={faHouse} className='icon active' />
@@ -21,9 +33,11 @@ const Navbar: React.FC = () => {
               : (
                 <img src={HomeIcon} alt="Home button" className="icon" />
                 )}
+          </motion.div>
           </Link>
         </div>
         <div className="navbar-item">
+        <motion.div whileTap={shouldAnimate('/search')} >
           <Link to="/search">
             {location.pathname === '/search'
               ? (
@@ -33,8 +47,10 @@ const Navbar: React.FC = () => {
                 <img src={SearchIcon} alt="Search button" className="icon" />
                 )}
           </Link>
+          </motion.div>
         </div>
         <div className="navbar-item">
+          <motion.div whileTap={shouldAnimate('/library')} >
           <Link to="/library">
             {location.pathname === '/library'
               ? (
@@ -44,8 +60,10 @@ const Navbar: React.FC = () => {
                 <img src={LibraryIcon} alt="Library icon" className="icon" />
                 )}
           </Link>
+          </motion.div>
         </div>
         <div className="navbar-item">
+          <motion.div whileTap={shouldAnimate('/user')} >
           <Link to="/user">
             {location.pathname === '/user'
               ? (
@@ -55,6 +73,7 @@ const Navbar: React.FC = () => {
                 <img src={ProfileIcon} alt="User icon" className="icon" />
                 )}
           </Link>
+          </motion.div>
         </div>
       </nav>
     </>
