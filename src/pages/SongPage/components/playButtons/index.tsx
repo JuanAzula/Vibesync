ef4a60019218f6aa01e1915113c9011372a97382
JuanAzula <141027910+JuanAzula@users.noreply.github.com>
2024-04-17 20:56:09 +0200

    Update toast config

--- src/pages/SongPage/components/playButtons/index.tsx
+++ src/pages/SongPage/components/playButtons/index.tsx
@@ -6,6 +6,7 @@ import { useLikedTracksContext } from '../../../../hooks/useLikedSongs'
 import { StyledLink } from '../../../../styledComponents/styledLink'
 import { useAudioContext } from '../../../../hooks/useAudio'
 import { type Track } from '../../../../types/data'
+import { toast } from 'sonner'
 
 interface Props {
   togglePlay: () => void
@@ -23,7 +24,6 @@ export const PlayButtons = ({ togglePlay, isPlaying, toggleMute, isMuted, track,
     audioImg,
     trackId
   } = useAudioContext()
-
   const checkTracksinLikedTracks = (track: Track) => {
     if (likedTracks[0] === null) {
       return false
@@ -36,26 +36,35 @@ export const PlayButtons = ({ togglePlay, isPlaying, toggleMute, isMuted, track,
 
   const handleHeartClick = () => {
     if (checkTracksinLikedTracks(track)) {
-      removeFromLikedTracks(track)
+      toast('Are you sure you want to remove this track from your liked songs?', {
+        action: {
+          label: 'Accept',
+          onClick: () => removeFromLikedTracks(track),
+        },
+        duration: 3000
+      })
     } else {
       addToLikedTracks(track)
+      toast('Track added to your liked songs', {
+        icon: '✓'
+      })
     }
   }
   return (
 
     <>
-        <div className='track-img'>
-          <StyledLink to={`tracks/${trackId}`}>
+      <div className='track-img'>
+        <StyledLink to={`tracks/${trackId}`}>
           <img src={audioImg} className='mini-player-img' />
-          </StyledLink>
-        </div>
+        </StyledLink>
+      </div>
       <div className='button-container'>
         <div className='button-container__handle-player'>
-        <FontAwesomeIcon icon={isMuted ? faVolumeMute : faVolumeUp} onClick={toggleMute} />
-        <FontAwesomeIcon icon={faBackwardStep} onClick={handlePreviousTrack} />
-        <FontAwesomeIcon icon={isPlaying ? faPause : faPlay} onClick={togglePlay} />
-        <FontAwesomeIcon icon={faForwardStep} onClick={handleNextTrack} />
-        <FontAwesomeIcon icon={ checkTracksinLikedTracks(track) ? faHeart : faHeartRegular} onClick={handleHeartClick} />
+          <FontAwesomeIcon icon={isMuted ? faVolumeMute : faVolumeUp} onClick={toggleMute} />
+          <FontAwesomeIcon icon={faBackwardStep} onClick={handlePreviousTrack} />
+          <FontAwesomeIcon icon={isPlaying ? faPause : faPlay} onClick={togglePlay} />
+          <FontAwesomeIcon icon={faForwardStep} onClick={handleNextTrack} />
+          <FontAwesomeIcon icon={checkTracksinLikedTracks(track) ? faHeart : faHeartRegular} onClick={handleHeartClick} />
         </div>
       </div>
     </>
