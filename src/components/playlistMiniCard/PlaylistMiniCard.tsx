f2428e3890c6fde395e2ba35fbcb9a806a26e836
JuanAzula <141027910+JuanAzula@users.noreply.github.com>
2024-04-24 16:47:56 +0200

    Fix merge issue

--- src/components/playlistMiniCard/PlaylistMiniCard.tsx
+++ src/components/playlistMiniCard/PlaylistMiniCard.tsx
@@ -1,4 +1,4 @@
-import './PlaylistMinicard.css'
+import './PlaylistMiniCard.css'
 
 interface Props {
   img: string
@@ -8,8 +8,8 @@ interface Props {
 export const PlaylistMiniCard = ({ img, title }: Props) => {
   return (
     <div className="playlist-minicard-container">
-        <img className="playlist-minicard-img" src={img} />
-        <p>{title}</p>
+      <img className="playlist-minicard-img" src={img} />
+      <p>{title}</p>
     </div>
   )
 }
